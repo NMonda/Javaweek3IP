c4670b26bd341f4a7a18ef5abc01a3f04444ee8f
NMONDA <ndegemonda@gmail.com>
2023-07-07 15:34:40 +0300

    Sighting_DAO patterns added, WITH LINKS TO ADDING AND DELETING AN Animal to a db

--- src/main/java/ke/co/pookie/dao/Sighting_DAO.java
+++ src/main/java/ke/co/pookie/dao/Sighting_DAO.java
@@ -1,4 +1,54 @@
 package ke.co.pookie.dao;
+import ke.co.pookie.database.Database;
+import ke.co.pookie.models.Animals;
+import ke.co.pookie.models.Sighting;
+import org.sql2o.Connection;
 
+import java.util.List;
 public class Sighting_DAO {
+
+    //CREATES  SIGHTINGS TABLE
+    public static void createTable (){
+        try(Connection db = Database.getConnection().open()){
+            String tableSpell = "CREATE TABLE IF NOT EXISTS sightings (id serial, animal varchar, endangered boolean default false, health varchar, age varchar, location varchar, ranger varchar, time timestamptz, deleted boolean default false);";
+            db.createQuery(tableSpell).executeUpdate();
+        } catch (Exception error) {System.out.println(error.getMessage());}
+    }
+    // NEW ANIMAL SIGHTING ADDED TO THE TABLE
+    public static void addSighting (Sighting newSighting) {
+        try (Connection db = Database.getConnection().open()) {
+            String addingSpell = "INSERT INTO sightings (animal, endangered, age, health, location, ranger, time) VALUES (UPPER(:animal), :endangered, :age, :health, :location, UPPER(:ranger), :time);";
+            db.createQuery(addingSpell).bind(newSighting).executeUpdate();
+        } catch (Exception error) { System.out.println(error.getMessage());}
+    }
+
+    //SELECTS A PARTICULAR ANIMAL FROM THE SIGHTINGS TABLE
+    public static List<Sighting> distinctSightings (String animal){
+        List<Sighting> specialSighting = null;
+        try (Connection db = Database.getConnection().open()){
+            String distinctSpell = "SELECT * FROM sightings WHERE animal = (:animal) AND NOT deleted;";
+            specialSighting = db.createQuery(distinctSpell).addParameter("animal", animal).executeAndFetch(Sighting.class);
+        } catch (Exception error){System.out.println(error.getMessage());}
+        return specialSighting;
+    }
+
+    //RETURNS ALL SIGHTED ANIMALS
+    public static List<Sighting> allSightings() {
+        List<Sighting> everySighting = null;
+        try (Connection db = Database.getConnection().open()){
+            String sightingsSpell = "SELECT * FROM sightings WHERE NOT deleted;";
+            everySighting = db.createQuery(sightingsSpell).executeAndFetch(Sighting.class);
+        } catch (Exception error){System.out.println(error.getMessage());}
+        return everySighting;
+    }
+
+    public static void deleteSighting(String animal){
+        try(Connection db = Database.getConnection().open()){
+            String deletionSpell = " UPDATE sightings SET deleted = true WHERE animal = (:animal);";
+            db.createQuery(deletionSpell).addParameter("animal", animal).executeUpdate();
+        } catch (Exception error) { System.out.println(error.getMessage());}
+    }
+
+
+
 }
