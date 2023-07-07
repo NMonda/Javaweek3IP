795a8c8bff07009990ced7e54078fdf847e48269
NMONDA <ndegemonda@gmail.com>
2023-07-07 13:36:24 +0300

    added Database.java content

--- src/main/java/ke/co/pookie/database/Database.java
+++ src/main/java/ke/co/pookie/database/Database.java
@@ -1,4 +1,17 @@
 package ke.co.pookie.database;
 
+import org.sql2o.Sql2o;
+
 public class Database {
-}
+        //connecting database
+    private static final Sql2o connection = new Sql2o (
+
+                "jdbc:postgresql://localhost:5432/wildlife_tracker_db",
+                "postgres",
+                "01234"
+        );
+
+    public static Sql2o getConnection() { return connection; }
+
+    }
+
